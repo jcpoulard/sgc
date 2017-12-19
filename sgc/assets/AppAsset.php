1525fa52afd2f6d9051aaf3b43f3cf0c343823e5
Jean Came Poulard <jcpoulard@gmail.com>
2017-12-19 11:45:15 -0500

    Integration du theme AdminLTE dans SGC

--- sgc/assets/AppAsset.php
+++ sgc/assets/AppAsset.php
@@ -15,12 +15,23 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
+/**
+ * @author Jean Came <jcpoulard@gmail.com>
+ */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        //'css/bootstrap.min.css',
+        'css/font-awesome/css/font-awesome.min.css',
+        'css/Ionicons/css/ionicons.min.css',
+        'css/jquery-jvectormap.css',
+        'css/AdminLTE.min.css',
+        'css/skins/skin-red-light.min.css',
+        
+        
     ];
     public $js = [
     ];
